739d666076714916edae9f198f1d356e3de44394
mdgordo <mdgordo@gmail.com>
2021-12-04 21:55:07 -0500

    cisse and barrett code with residual estimation

--- code/MSM.R
+++ code/MSM.R
@@ -11,83 +11,89 @@ source(paste(getwd(), "/code/VFIfunctions.r", sep = ""))
 gamma <- 3
 beta <- .95
 R <- 1.03
-alpha = .3
-mu = 11
-sigma = .35
 cmin = 10000
 lambda = .2
+sigma = 3.85
 
-theta <- c(gamma, beta, R, alpha, mu, sigma, cmin, lambda)
+theta <- c(gamma, beta, R, cmin, lambda, sigma)
+
+u <- function(x, gamma){x^(1-gamma)/(1-gamma)}
 
 ### we want to match on non durable consumption and non transfer income - percapita?
 df <- filter(df.hh, !is.na(income_gross) & !is.na(lag_income_gross) & !is.na(age_hh) & !is.na(class5))
 cissebarret <- lm(log(income_gross+1) ~ as.factor(caste_recode) + poly(age_hh, 2) + femalehh + class5 + class10 +
-                    slope + elevation + poly(log(lag_income_gross+1), 3) + as.factor(land_qtle),
-                  data = df, weights = wt_hh, na.action = na.omit)
+                    slope + elevation + as.factor(land_qtle) #+ poly(log(lag_income_gross+1), 3) 
+                  , data = df, weights = wt_hh, na.action = na.omit)
 winc_guess <- as.vector(coef(cissebarret))
 
 df$resid <- cissebarret$residuals^2
 cissebarret_resid <- lm(resid ~ as.factor(caste_recode) + poly(age_hh, 2) + femalehh + class5 + class10 +
-                    slope + elevation + poly(log(lag_income_gross+1), 3) + as.factor(land_qtle),
-                  data = df, weights = wt_hh, na.action = na.omit)
+                          slope + elevation + as.factor(land_qtle) #+ poly(log(lag_income_gross+1), 3) 
+                        , data = df, weights = wt_hh, na.action = na.omit)
 wresid_guess <- as.vector(coef(cissebarret_resid))
-### need to use to predict income and variance - what moments to match here
 
-### PDF Moments
+#ggplot() +geom_histogram(aes(x = df$mu), bins = 100)
+#ggplot() +geom_histogram(aes(x = df$sigma), bins = 100)
+#ggplot() +geom_point(aes(x = df$mu, y = df$sigma))
+#ggplot() +geom_point(aes(x = log(df$income_gross+1), y = cissebarret$residuals^2))
 
-empiricalmoments <- c(0, .04, .25, .52, .64, .77, .86)
+### coarsen for feasibility - distributions should be similar across hh years because most of w vars don't change
+df$mu <- round(cissebarret$fitted.values,1)
+df$sigma <- round(sqrt(cissebarret_resid$fitted.values),)
 
-### utility and production functional forms
-u <- function(x, gamma){x^(1-gamma)/(1-gamma)}
-f <- function(k, epsilon, alpha) {epsilon*k^alpha}
+musig <- unique(cbind(df$mu, df$sigma))
+
+### Moments
+
+pdfmoments_t <- c(0, .04, .25, .52, .64, .77, .86)
+pdfmoments_u <- c(0.05, 0.18, 0.32, 0.57, 0.63, 0.79, 0.77, 0.86, 0.92)
+mkt_clear <- 0
+
+moments <- c(pdfmoments_t, pdfmoments_u, mkt_clear)
 
 ### Optimization setup
-ROI_setup <- function(Vfx, x, theta, epsilon, B, roots = FALSE){
-  objective <- function(ck) {
-    c <- ck[1]
-    k <- ck[2]
-    xtplus1 = theta[3]*(x - c - k) + f(k, epsilon, theta[4])
-    r = u(c, theta[1]) + theta[2]*mean(Vfx(xtplus1))
-    return(r)
+bellman_operator <- function(grid, w, B, beta, R, mu, sigma, gamma, cmin){
+  Valfunc = approxfun(grid, w, rule = 2)
+  y = rlnorm(1000, meanlog = mu, sd = sigma)
+  optimizer <- function(x){
+    if (x==B) {return(u(cmin, gamma) + beta*mean(Valfunc(B)))} else{
+      objective <- function(c) {
+        xtplus1 = R*(x - c) + y
+        xtplus1 = if_else(xtplus1<B, B, xtplus1)
+        r = u(c, gamma) + beta*mean(Valfunc(xtplus1))
+        return(r)
+      }
+      l <- optimize(objective, interval = c(1, x - B), maximum = TRUE)
+      return(l$objective)
+    }
   }
-  ### has to hold with certainty, so min of production
-  constraint <- function(ck){
-    c <- ck[1]
-    k <- ck[2]
-    return(B - theta[3]*(x - c - k) - min(f(k, epsilon, theta[4])))
-  }
-  nlprob <- OP(F_objective(objective, 2),
-               F_constraint(constraint, dir = "<=", rhs = 0),
-               maximum = TRUE,
-               bounds = V_bound(li = c(1,2), lb = c(1,1)))
-  r <- ROI_solve(nlprob, solver = "nloptr.cobyla", start = c(1.1, 1.1))
-  if (roots=TRUE) return(solution(r)) else return(objective(solution(r)))
-}
-
-bellman_operator <- function(grid, w, theta, epsilon, B){
-  Vfx = approxfun(grid, w, rule = 2)
   Tw = rep(NA, length(grid))
-  Tw = mclapply(xgrid, ROI_setup, Vfx = Vfx, theta, epsilon, B, mc.cores = 6, mc.preschedule = FALSE)
+  Tw = mclapply(grid, optimizer, mc.cores = 6)
   return(unlist(Tw))
 }
 
-#grid = xgrid; vinit = v0; tol = 1e-13; maxiter = 300
-
-VFI <- function(grid, vinit, tol = 1e-13, maxiter = 300, theta, epsilon, B){
+VFI <- function(grid, vinit, tol = 1e-9, maxiter = 300, B, beta, R, mu, sigma, gamma, cmin){
   w = matrix(0, length(grid), 1)
   w[,1] = vinit
   d = 1
   i = 2
   while (d > tol & i < maxiter){
     w = cbind(w, rep(0, length(grid)))
-    w[,i] = bellman_operator(grid, w[,i-1], theta, epsilon, B)
+    w[,i] = bellman_operator(grid, w[,i-1], B, beta, R, mu, sigma, gamma, cmin)
     d = sqrt(sum((w[,i] - w[,i-1])^2))
     i = i+1
   }
   return(w)
 }
 
+
+### MSM
+
 msm_func <- function(theta){
+  gamma = theta[1]; beta = theta[2]; R = theta[3]; cmin = theta[4]; lambda = theta[5]
+  
+  ### Solve for all the value functions
+  
   ### productivity draws and lower bound
   epsilon <- rlnorm(1000, theta[5], theta[6])
   B <- -min(f(1, epsilon, theta[4]))/(theta[3]-1)
